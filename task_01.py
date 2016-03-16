8fc42aa22d7878c75b0bb8c21b288b43a20c1196
paisely65 <cunysps@gmail.com>
2016-03-16 15:10:51 -0400

    Task I

---
+++ task_01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A docstring to provide brnaching for decision trees."""
+
+BASE = raw_input('Which base colors are you using? ')
+BASE_CHOICE = 'I will use: {0}, {1}.'
+ACCENT = raw_input('Which accent will you use? ')
+ACCENT_CHOICE = 'I will use: {0}, {1}'
+HIGHLIGHT = raw_input('Which highlight color are you using? ')
+HIGHLIGHT_CHOICE = 'I will use: {0}, {1}.'
