719105626300156ee2118265ca0baa8087079997
paisely65 <cunysps@gmail.com>
2016-03-18 19:52:02 -0400

    Task 3 is finished.

--- task_03.py
+++ task_03.py
@@ -10,7 +10,7 @@ PRINCIPAL = int(raw_input('What is the amount of your principal? '))
 
 DURATION = int(raw_input('For how many years is this loan being borrowed? '))
 
-PRE_QUALIFIED = raw_input('Are you pre qualified for this loan? ')
+PRE_QUALIFIED = raw_input('Are you pre-qualified for this loan? ')
 
 RATE = None
 
@@ -18,41 +18,41 @@ TOTAL = None
 
 if 0 <= PRINCIPAL and PRINCIPAL <= 199999:
     if 1 <= DURATION and DURATION <= 15:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
             RATE = '0.0363'
-        elif PRE_QUALIFIED == 'No':
+        elif PRE_QUALIFIED == 'No' or PRE_QULAIFIED == 'n':
             RATE = '0.0465'
     elif 16 <= DURATION and DURATION <= 20:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
             RATE = '0.0404'
-        elif PRE_QUALIFIED == 'No':
+        elif PRE_QUALIFIED == 'No' or PRE_QUALIFIED == 'n':
             RATE = '0.0498'
     elif 21 <= DURATION and DURATION <= 30:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFICATION == 'y':
             RATE = '0.0577'
-        elif PRE_QUALIFIED == 'No':
+        elif PRE_QUALIFIED == 'No'or PRE_QUALIFIED == 'n':
             RATE = '0.0639'
 elif 200000 <= PRINCIPAL and PRINCIPAL <= 999999:
     if 1 <= DURATION and DURATION <= 15:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
             RATE = '0.0302'
-        elif PRE_QUALIFIED == 'No':
+        elif PRE_QUALIFIED == 'No' or PRE_QUALIFIED == 'n':
             RATE = '0.0398'
     elif 16 <= DURATION and DURATION <= 20:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
             RATE = '0.0327'
-        elif PRE_QUALIFIED == 'No':
+        elif PRE_QUALIFIED == 'No' or PRE_QUALIFIED == 'n':
             RATE = '0.0408'
     elif 21 <= DURATION and DURATION <= 30:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
             RATE = '0.0466'
 elif 1000000 <= PRINCIPAL:
     if 1 <= DURATION and DURATION <= 15:
-        if PRE_QUALIFIED == 'Yes':
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
             RATE = '0.0205'
     elif 16 <= DURATION and DURATION <= 20:
-        if PRE_QUALIFIED == 'Yes':
-            RATE = '0.0261'
+        if PRE_QUALIFIED == 'Yes' or PRE_QUALIFIED == 'y':
+            RATE = '0.0262'
 N = 12
 if RATE is not None:
     RATE = decimal.Decimal(RATE)
@@ -63,9 +63,11 @@ else:
 print ''
 print 'Loan Report For: {}'.format(NAME)
 print '---------------------------------------'
-print '     Principle:{:>15}'.format(PRINCIPAL)
-print '     Duration:{:>13}'.format(DURATION)+ 'years'
-print '     Pre-Qualified?:{:>10}'.format(PRE_QUALIFIED)
+print '      Principle:{:>15}'.format(PRINCIPAL)
+print '      Duration:{:>13}'.format(DURATION)+'years'
+print '      Pre-qualified?:{:>10}'.format(PRE_QUALIFIED)
 print''
 if TOTAL is not None:
-    print 'Total:{:>19.0f}'.format(TOTAL)
+    print '      Total:{:>19.0f}'.format(TOTAL)
+else:
+    print '      Total:{:>20'.format(None)
