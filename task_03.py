f601e4d8af5fc198a42d1ed4ea666228addd7c27
Wei Chen <leo.c.1114@gmail.com>
2016-03-20 23:38:23 +0000

    modified task 3 file

--- task_03.py
+++ task_03.py
@@ -52,15 +52,21 @@ if PRINCIPLE >= 1000000:
         if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
             IR = '0.0262'
 
+N = 12
+
 if IR is not None:
     IR = decimal.Decimal(IR)
-    
-if TOTAL is not None:
-    TOTAL = int(round(PRINCIPLE * (1 + IR/12)**(12*YEARS)))
+    TOTAL = int(round(PRINCIPAL * (1 + IR/N)**(N*YEARS)))
+else:
+    TOTAL = None
+
 
 print 'Loan Report for: {0}'.format(NAME)
 print '-' * 35
 print '        Principle:{:>12}'.format(PRINCIPLE)
 print '        Duration:{:>13}'.format(YEARS)
 print '        Pre-qualified:{:>8}'.format(PQUALIFIED)
-print '        Total:{:>16}'.format(TOTAL)
+if TOTAL is not None:
+    print '        Total:{:>16}'.format(TOTAL)
+else:
+    print '        Total:{:>16}'.format(None)
