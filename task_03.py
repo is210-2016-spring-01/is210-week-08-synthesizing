8adcadde74be4079da9add7bcfa02e2cdd79c47c
Wei Chen <leo.c.1114@gmail.com>
2016-03-21 08:46:39 +0000

    modified task 3 file

--- task_03.py
+++ task_03.py
@@ -5,66 +5,81 @@
 import decimal
 
 NAME = raw_input('What is your name? ')
-PRINCIPLE = int(raw_input('What is the principle of the loan? '))
+PRINCIPAL = int(raw_input('What is the principle of the loan? '))
 YEARS = int(raw_input('For how long is this being borrowed? '))
-PQUALIFIED = raw_input('Are you prequalified? ')
+PQ = raw_input('Are you prequalified? ')
 
-IR = None
-TOTAL = None
+IR = 0
+TOTAL = 0
 
-if PRINCIPLE >= 0 and PRINCIPLE <= 199999:
-    if YEARS >= 1 and YEARS <= 15:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0363'
-        elif PQUALIFIED == ('No', 'n'):
-            IR = '0.0465'
-    elif YEARS >= 16 and YEARS <= 20:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0404'
-        elif PQUALIFIED == ('No', 'n'):
-            IR = '0.0498'
-    elif YEARS >= 21 and YEARS <= 30:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0577'
-        elif PQUALIFIED == ('No', 'n'):
-            IR = '0.0639'
+if 0 <= PRINCIPAL <= 199999:
+    if YEARS <= 15:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0363'
+        else:
+            IR = '.0465'
+    elif 16 <= YEARS <= 20:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0404'
+        else:
+            IR = '.0498'
+    elif 21 <= YEARS <= 30:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0577'
+        else:
+            IR = '.0639'
+    else:
+        IR = None
 
-if PRINCIPLE >= 200000 and PRINCIPLE <= 999999:
-    if YEARS >= 1 and YEARS <= 15:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0302'
-        elif PQUALIFIED == ('No', 'n'):
-            IR = '0.0398'
-    elif YEARS >= 16 and YEARS <= 20:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0327'
-        elif PQUALIFIED == ('No', 'n'):
-            IR = '0.0408'
-    elif YEARS >= 21 and YEARS <= 30:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0466'
+elif 200000 <= PRINCIPAL <= 999999:
+    if YEARS <= 15:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0302'
+        else:
+            IR = '.0398'
+    elif 16 <= YEARS <= 20:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0327'
+        else:
+            IR = '.0408'
+    elif 21 <= YEARS <= 30:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0466'
+        else:
+            IR = None
+    else:
+        IR = None
 
-if PRINCIPLE >= 1000000:
-    if YEARS >= 1 and YEARS <= 15:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0205'
-    if YEARS >= 16 and YEARS <= 20:
-        if PQUALIFIED == ('Yes', 'y'):
-            IR = '0.0262'
-
-N = 12
+elif PRINCIPAL >= 1000000:
+    if YEARS <= 15:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0205'
+        else:
+            IR = None
+    elif 16 <= YEARS <= 20:
+        if PQ == 'Yes' or PQ == 'y':
+            IR = '.0262'
+        else:
+            IR = None
+    else:
+        IR = None
+else:
+    IR = None
 
 if IR is not None:
     IR = decimal.Decimal(IR)
-
-if TOTAL is not None:
-    TOTAL = int(round(PRINCIPLE * (1 + IR/N)**(N*YEARS)))
+    TOTAL = int(round(PRINCIPAL * (1 + IR / 12) ** (12 * YEARS)))
+else:
+    TOTAL = None
 
 print ''
 print 'Loan Report for: {0}'.format(NAME)
 print '-' * 35
-print '      Principle:{:>15}'.format(PRINCIPLE)
+print '      Principle:{:>15}'.format(PRINCIPAL)
 print '      Duration:{:>16}'.format(YEARS)
-print '      Pre-qualified?:{:>10}'.format(PQUALIFIED)
+print '      Pre-qualified?:{:>10}'.format(PQ)
 print ''
-print '      Total:{:>19}'.format(TOTAL)
+if TOTAL is not None:
+    print '      Total:{:>19}'.format(TOTAL)
+else:
+    print '      Total:{:>19}'.format('None')
