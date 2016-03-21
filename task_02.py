b8b16f9c22688ca6fb7aa34bf5f2e8b86055939f
Sylvia Guzikowski <xiran-aneki@live.com>
2016-03-21 13:19:49 +0000

    it was a spelling error... NOW THIS ONE

--- task_02.py
+++ task_02.py
@@ -3,8 +3,8 @@
 """Second task for this week"""
 
 DAY = raw_input('What day is it? ')
-TIME = raw_input('What time to start alarm? ')
-TIME = int(TIME)
+TIME = int(raw_input('What time to start alarm? '))
+
 
 SNOOZE = True if DAY == 'sat' or DAY == 'sun' or TIME < 600 else False
 
