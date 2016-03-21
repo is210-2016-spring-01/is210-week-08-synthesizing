c2d20d44168b242e5a30ae05c36a6ba13ac5e57a
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-21 13:25:19 +0000

    *sigh*

--- task_03.py
+++ task_03.py
@@ -16,45 +16,69 @@ PREQUALIFIED = raw_input("Are you pre-qualified for this loan? ")
 if PRINCIPAL > 0 and PRINCIPAL < 200000:
     if DURATION > 1 and DURATION <= 15:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 3.63
+            RATE = decimal.Decimal(3.63) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
         elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
-            INTEREST = 4.65
+            RATE = decimal.Decimal(4.65) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
     elif DURATION > 15 and DURATION <= 20:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 4.04
+            RATE = decimal.Decimal(4.04) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
         elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
-            INTEREST = 4.98
+            RATE = decimal.Decimal(4.98) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
     elif DURATION > 20 and DURATION <= 30:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 5.77
+            RATE = decimal.Decimal(5.77) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
         elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
-            INTEREST = 6.39
+            RATE = decimal.Decimal(6.39) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
 elif PRINCIPAL >= 200000 and PRINCIPAL <= 999999:
     if DURATION > 1 and DURATION <= 15:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 3.02
+            RATE = decimal.Decimal(3.02) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
         elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
-            INTEREST = 3.98
+            RATE = decimal.Decimal(3.98) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
     elif DURATION > 15 and DURATION <= 20:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 3.27
+            RATE = decimal.Decimal(3.27) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
         elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
-            INTEREST = 4.08
+            RATE = decimal.Decimal(4.08) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
     elif DURATION > 20 and DURATION <= 30:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 4.66
+            RATE = decimal.Decimal(4.66) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
 elif PRINCIPAL > 1000000:
     if DURATION > 1 and DURATION <= 15:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 2.05
+            RATE = decimal.Decimal(2.05) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
     elif DURATION > 15 and DURATION <= 20:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
-            INTEREST = 2.62
+            RATE = decimal.Decimal(2.62) / 100
+            TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (
+                12 * DURATION))))
 else:
     TOTAL = None
 
-RATE = decimal.Decimal(INTEREST) / 100
-TOTAL = int(round(PRINCIPAL * ((1 + (RATE / 12)) ** (12 * DURATION))))
 
 REPORT = ("Loan Report for: {}\n" +
           "----------------------------------------------\n"
