a6d606a9a1872f3ed1130c7f34ba3db54d4b6e70
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-21 10:13:46 +0000

    hmmmm

--- task_02.py
+++ task_02.py
@@ -21,7 +21,7 @@ WEEKEND = DAY == 'sun' or DAY == 'sat'
 NIGHTTIME = TIME < 0600 or TIME < 600
 
 SNOOZE = True if WEEKEND or NIGHTTIME else False
-    
+
 if SNOOZE is False:
     TIME >= 600
     ALARM = "Beep! " * 5
