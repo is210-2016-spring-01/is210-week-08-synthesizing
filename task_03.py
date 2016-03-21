51a2b5ca17a64e9e91d479af792c679720078aec
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-21 11:16:33 +0000

    hmmmm

--- task_03.py
+++ task_03.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 03"""
+"""Task 03: mortgage calculator to calculate the
+lifetime compound interest of a loan."""
 
 
 import decimal
-# A=P(1+\frac{r}{n})^{nt}
 
 NAME = raw_input("What is your name? ")
 PRINCIPAL = int(raw_input(
@@ -16,57 +16,52 @@ if PRINCIPAL > 0 and PRINCIPAL < 200000:
     if DURATION > 1 and DURATION <= 15:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 3.63
-        if PREQUALIFIED == "No" or PREQUALIFIED == "n":
+        elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
             INTEREST = 4.65
-    if DURATION > 15 and DURATION <= 20:
+    elif DURATION > 15 and DURATION <= 20:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 4.04
-        if PREQUALIFIED == "No" or PREQUALIFIED == "n":
+        elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
             INTEREST = 4.98
-    if DURATION > 20 and DURATION <= 30:
+    elif DURATION > 20 and DURATION <= 30:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 5.77
-        if PREQUALIFIED == "No" or PREQUALIFIED == "n":
+        elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
             INTEREST = 6.39
-if PRINCIPAL >= 200000 and PRINCIPAL <= 999999:
+elif PRINCIPAL >= 200000 and PRINCIPAL <= 999999:
     if DURATION > 1 and DURATION <= 15:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 3.02
-        if PREQUALIFIED == "No" or PREQUALIFIED == "n":
+        elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
             INTEREST = 3.98
-    if DURATION > 15 and DURATION <= 20:
+    elif DURATION > 15 and DURATION <= 20:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 3.27
-        if PREQUALIFIED == "No" or PREQUALIFIED == "n":
+        elif PREQUALIFIED == "No" or PREQUALIFIED == "n":
             INTEREST = 4.08
-    if DURATION > 20 and DURATION <= 30:
+    elif DURATION > 20 and DURATION <= 30:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 4.66
-if PRINCIPAL > 1000000:
+elif PRINCIPAL > 1000000:
     if DURATION > 1 and DURATION <= 15:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 2.05
-    if DURATION > 15 and DURATION <= 20:
+    elif DURATION > 15 and DURATION <= 20:
         if PREQUALIFIED == "Yes" or PREQUALIFIED == "y":
             INTEREST = 2.62
 else:
     TOTAL = None
 
+INTEREST = decimal.Decimal(INTEREST)
+TOTAL = round(PRINCIPAL * (1 + (INTEREST / 12)) ** (12 * DURATION))
 
-# A=P(1+\frac{r}{n})^{nt}
-P = PRINCIPAL
-r = INTEREST
-n = 12
-t = DURATION
-
-TOTAL = P(1+(r)/(n)) ** (nt)
-print round(TOTAL)
+print INTEREST
 
 REPORT = ("Loan Report for: {}\n" +
           "----------------------------------------------\n"
           + "    Principal: ${}\n" + "    Duration: {}yrs\n" +
           "    Pre-Qualified?: {}\n\n" +
           "    Total: ${}").format(NAME, PRINCIPAL,
-                               DURATION, PREQUALIFIED, TOTAL)
+                                   DURATION, PREQUALIFIED, TOTAL)
 
 print REPORT
