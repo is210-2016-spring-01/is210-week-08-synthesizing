104c09e75fdff6bd9667c3b89827e4243bba2920
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-21 06:04:59 +0000

    hmmmm

--- task_02.py
+++ task_02.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 02"""
+"""Task 02: Build a simple alarm clock with snooze function"""
 
 
 DAY = raw_input("What day is it? ")
-TIME = raw_input("What time is it? ")
-
-if len(TIME) != 4:
-    raise ValueError()
-TIME = int(TIME)
+TIME = int(raw_input("What time is it (as a four digit number)? "))
 
 if len(DAY) > 3:
     DAY = DAY.lower()
     DAY = DAY[0:3]
-if len(DAY) < 3:
-    raise ValueError()
 
-WEEKEND = DAY == 'sun' or DAY == 'sat'
-NIGHTTIME = TIME < 0600 or TIME < 600
 
-SNOOZE = True if WEEKEND or NIGHTTIME else False
+WEEKEND = (DAY == 'sun') or (DAY == 'sat')
+NIGHTTIME = (TIME > 000 and TIME < 0600) or (TIME > 000 and TIME < 600)
 
-if False:
-    ALARM = "BEEP!" * 5
-    print ALARM
+SNOOZE = True if (WEEKEND) or (NIGHTTIME) else False
 
-print SNOOZE
+if SNOOZE == False:
+    print "Beep! " * 5
