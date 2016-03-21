a60ad6cddb8f9fb4e5795b011accbf6796b8b388
Wei Chen <leo.c.1114@gmail.com>
2016-03-21 00:21:44 +0000

    modified task 3

--- task_03.py
+++ task_03.py
@@ -5,8 +5,8 @@
 import decimal
 
 NAME = raw_input('What is your name? ')
-PRINCIPLE = raw_input('What is the principle of the loan? ')
-YEARS = raw_input('For how long is this being borrowed? ')
+PRINCIPLE = int(raw_input('What is the principle of the loan? '))
+YEARS = int(raw_input('For how long is this being borrowed? '))
 PQUALIFIED = raw_input('Are you prequalified? ')
 
 IR = None
@@ -14,42 +14,42 @@ TOTAL = None
 
 if PRINCIPLE >= 0 and PRINCIPLE <= 199999:
     if YEARS >= 1 and YEARS <= 15:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0363'
-        elif PQUALIFIED == 'No' or PQUALIFIED == 'n':
+        elif PQUALIFIED == ('No', 'n'):
             IR = '0.0465'
     elif YEARS >= 16 and YEARS <= 20:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0404'
-        elif PQUALIFIED == 'No' or PQUALIFIED == 'n':
+        elif PQUALIFIED == ('No', 'n'):
             IR = '0.0498'
     elif YEARS >= 21 and YEARS <= 30:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0577'
-        elif PQUALIFIED == 'No' or PQUALIFIED == 'n':
+        elif PQUALIFIED == ('No', 'n'):
             IR = '0.0639'
 
 if PRINCIPLE >= 200000 and PRINCIPLE <= 999999:
     if YEARS >= 1 and YEARS <= 15:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0302'
-        elif PQUALIFIED == 'No' or PQUALIFIED == 'n':
+        elif PQUALIFIED == ('No', 'n'):
             IR = '0.0398'
     elif YEARS >= 16 and YEARS <= 20:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0327'
-        elif PQUALIFIED == 'No' or PQUALIFIED == 'n':
+        elif PQUALIFIED == ('No', 'n'):
             IR = '0.0408'
     elif YEARS >= 21 and YEARS <= 30:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0466'
 
 if PRINCIPLE >= 1000000:
     if YEARS >= 1 and YEARS <= 15:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0205'
     if YEARS >= 16 and YEARS <= 20:
-        if PQUALIFIED == 'Yes' or PQUALIFIED == 'y':
+        if PQUALIFIED == ('Yes', 'y'):
             IR = '0.0262'
 
 N = 12
@@ -68,6 +68,6 @@ print '      Duration:{:>16}'.format(YEARS)
 print '      Pre-qualified?:{:>10}'.format(PQUALIFIED)
 print ''
 if TOTAL is not None:
-    print '      Total:{:>19.0f}'.format(TOTAL)
+    print '      Total:{:>19}'.format(TOTAL)
 else:
     print '      Total:{:>19}'.format(None)
