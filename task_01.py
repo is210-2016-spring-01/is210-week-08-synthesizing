718d16ca312572a156226c9f9a0d96964c53ab18
Sylvia Guzikowski <xiran-aneki@live.com>
2016-03-21 12:34:52 +0000

    ..i think i got it

--- task_01.py
+++ task_01.py
@@ -4,22 +4,22 @@
 
 BASE = raw_input('Which base color, "Seattle Gray" or "Manatee"?: ')
 if BASE == 'Seattle Gray':
-    ACCENT = raw_input('Which accent color, '
+    if ACCENT = raw_input('Which accent color, '
                        '"Ceramic Glaze" or "Cumulus Nimbus"?: ')
-    if ACCENT == 'Ceramic Glaze':
-        HIGHLIGHT = raw_input('Which highlight color'
+    elif ACCENT == 'Ceramic Glaze':
+        if HIGHLIGHT = raw_input('Which highlight color'
                               ', "Basically White" or "White"?: ')
     elif ACCENT == 'Cumulus Nimbus':
-        HIGHLIGHT = raw_input('Which highlight color'
+        if HIGHLIGHT = raw_input('Which highlight color'
                               ', "Off-White" or "Paper White"?: ')
 elif BASE == 'Manatee':
-    ACCENT = raw_input('Which accent color'
+    if ACCENT = raw_input('Which accent color'
                        ', "Platinum Mist" or "Spartan Sage"?: ')
-    if ACCENT == 'Platinume Mist':
-        HIGHLIGHT = raw_input('Which highlight color'
+    elif ACCENT == 'Platinume Mist':
+        if HIGHLIGHT = raw_input('Which highlight color'
                               ',"Bone White" or "Just White"?: ')
     elif ACCENT == 'Spartan Sage':
-        HIGHLIGHT = raw_input('Which highlight color'
+        if HIGHLIGHT = raw_input('Which highlight color'
                               ',"Fractal White" or "Not White"?: ')
 
 MYSTR = 'Your selected colors are, {0}, {1}, and {2}.'
