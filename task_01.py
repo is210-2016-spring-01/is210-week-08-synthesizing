b4afb1a7d9601742c7947065de2c32896bcfaf05
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-21 03:01:11 +0000

    upload changes

--- task_01.py
+++ task_01.py
@@ -5,22 +5,22 @@
 BASE = raw_input('Which base color, "Seattle Gray" or "Manatee"? ')
 
 if BASE == 'Seattle Gray':
-    ACCNT = raw_input('Which accent color,'
+    ACCENT = raw_input('Which accent color,'
                       '"Ceramic Glaze" or "Cumulus Nimbus"? ')
-    if ACCNT == 'Ceramic Glaze':
+    if ACCENT == 'Ceramic Glaze':
         HIGHLT = raw_input('Which highlight color,'
                            '"Basically White" or "White"? ')
     else:
         HIGHLT = raw_input('Which highlight color,'
                            '"Off-White" or "Paper White"? ')
 else:
-    ACCNT = raw_input('Which accent color,'
+    ACCENT = raw_input('Which accent color,'
                       '"Platinum Mist" or "Spartan Sage"? ')
-    if ACCNT == 'Platinum Mist':
+    if ACCENT == 'Platinum Mist':
         HIGHLT = raw_input('Which highlight color,'
                            '"Bone White" or "Just White"? ')
     else:
         HIGHLT = raw_input('Which highlight color,'
                            '"Fractal White" or "Not White"? ')
 
-print 'Your selected colors are, {0}, {1} and {2}.'.format(BASE, ACCNT, HIGHLT)
+print 'Your selected colors are, {0}, {1} and {2}.'.format(BASE, ACCENT, HIGHLT)
--- task_02.py
+++ task_02.py
@@ -4,6 +4,6 @@
 
 DAY = raw_input('What day is it? ')
 TIME = int(raw_input('What time is it? '))
-SNOOZE = (DAY.lower()[:3] is 'sat' or 'sun') and TIME < 600
+SNOOZE = (DAY.lower()[:3] is 'sat' or 'sun') or TIME < 600
 
 print 'Beep!' * 5 if not SNOOZE else True
