b0735f44751403bc3c50fcabb97296eb7e62d7e9
Sylvia Guzikowski <xiran-aneki@live.com>
2016-03-21 18:49:39 +0000

    i AM STILL TRYING

--- task_03.py
+++ task_03.py
@@ -16,63 +16,82 @@ RATE = None
 if LOAN >= 0 and LOAN <= 199999:
     if YEARS >= 1 and YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0363'
+            RATE = 0.0363
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
-            RATE = '0.0465'
+            RATE = 0.0465
     elif YEARS >= 16 and YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0404'
+            RATE = 0.0404
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
-            RATE = '0.0498'
+            RATE = 0.0498
     elif YEARS >= 21 and YEARS <= 30:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0577'
+            RATE = 0.0577
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
-            RATE = '0.0639'
+            RATE = 0.0639
 elif LOAN >= 200000 and LOAN <= 999999:
     if YEARS >= 1 and YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0302'
+            RATE = 0.0302
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
-            RATE = '0.0398'
+            RATE = 0.0398
     elif YEARS >= 16 and YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0327'
+            RATE = 0.0327
         if QUALIFIED == 'No' or QUALIFIED == 'n':
-            RATE = '0.0408'
+            RATE = 0.0408
     elif YEARS >= 21 and YEARS <= 30:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0466'
+            RATE = 0.0466
 elif LOAN >= 1000000:
     if YEARS >= 1 and YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0205'
+            RATE = 0.0205
     elif YEARS >= 16 and YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0262'
+            RATE = 0.0262
 
 N = 12
 if RATE is not None:
-   RATE = decimal.Decimal(RATE)
-   TOTAL = long(LOAN * (1 + RATE/N)**(N*YEARS))
+    TOTAL = int(LOAN * (1 + RATE/N)**(N*YEARS))
+
 else:
     TOTAL = None
 
-LINE1 = 'Principal:'
-LINE2 = 'Duration:'
-LINE3 = 'Pre-Qualified?:'
-REPORT = ('Loan report for: {0} \n'
-          + '-------------------------------------------------' + '\n'
-          + '       Principal:      ' + '${1}'.rjust(21) + '\n'
-          + '       Duration:             ' + '{2}'.rjust(20) + '\n'
-          + '       Pre-Qualified?:        ' + '{3}'.rjust(20) + '\n'
-          + '\n'
-          + '       Total:                ' + '${4}'.rjust(15))
-
-TOTAL = int(TOTAL)
-LOAN = int(LOAN)
 LOAN = '{:0,d}'.format(LOAN)
-TOTAL = '{:0,d}'.format(TOTAL)
+STOTAL = '{:0,d}'.format(TOTAL)
+
+REPORT = ('Loan report for: ' + NAME + ' \n'
+          + '-------------------------------------------------' + '\n'
+          + '       Principal:      ' + str(LOAN).rjust(21) + '\n'
+          + '       Duration:      ' + str(YEARS).rjust(22) + '\n'
+          + '       Pre-Qualified?:      ' + QUALIFIED.rjust(16) + '\n'
+          + '\n'
+          + '       Total:      ' + STOTAL.rjust(25))
+
+print REPORT
+# if RATE is not None:
+   # RATE = decimal.Decimal(RATE)
+   # TOTAL = int(LOAN * (1 + RATE/N)**(N*YEARS))
+# else:
+    # TOTAL = None
+
+# LINE1 = 'Principal:'
+# LINE2 = 'Duration:'
+# LINE3 = 'Pre-Qualified?:'
+# REPORT = ('Loan report for: {0} \n'
+          # + '-------------------------------------------------' + '\n'
+          # + '       Principal:      ' + '${1}'.rjust(21) + '\n'
+          # + '       Duration:             ' + '{2}'.rjust(20) + '\n'
+          # + '       Pre-Qualified?:        ' + '{3}'.rjust(20) + '\n'
+          # + '\n'
+          # + '       Total:                ' + '${4}'.rjust(15))
+
+
+# LOAN = '{:0,d}'.format(LOAN)
+# STOTAL = '{:0,d}'.format(TOTAL)
+#TOTAL = int(TOTAL)
+#LOAN = int(LOAN)
 
 # STOTAL = '{:0,d}'.format(TOTAL)
 # SLOAN = '{:0,d}'.format(LOAN)
@@ -81,4 +100,4 @@ TOTAL = '{:0,d}'.format(TOTAL)
 # TOTAL = float(TOTAL)
 # YEARS = float(YEARS)
 
-print REPORT.format(NAME, LOAN, YEARS, QUALIFIED, TOTAL)
+#print REPORT.format(NAME, LOAN, YEARS, QUALIFIED, STOTAL)
