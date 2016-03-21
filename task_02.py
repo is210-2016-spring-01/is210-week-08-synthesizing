c5f5ea0787638527a5aeab94823a8fcbf45d0f35
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-21 06:21:06 +0000

    rev 4

--- task_02.py
+++ task_02.py
@@ -4,7 +4,9 @@
 
 DAY = raw_input('What day is it? ')
 TIME = int(raw_input('What time is it? '))
-SNOOZE = (DAY.lower()[:3] == 'sat' or 'sun') or TIME < 600
-if SNOOZE == False:
-    print 'Beep' * 5
-
+DAY = DAY.lower()[:3]
+if DAY == 'sat' or DAY == 'sun' or (TIME < 0600)is True:
+    SNOOZE = True
+else:
+    SNOOZE = False
+    print 'Beep! ' * 5
