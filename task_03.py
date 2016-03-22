391e19e1419dd495981c8a19dfe6c5f2861b1ffb
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-22 23:03:32 +0000

    last rev

--- task_03.py
+++ task_03.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Total owned over life of loan."""
+"""Total owed over life of loan."""
 
 import decimal
 
 NAME = raw_input('What is your name? ')
-PRIN = int(raw_input('What is the amount of your principal (the amount being'
-                     ' borrowed? '))
+PRIN = int(raw_input('What is the amount of your principal?'))
 YEARS = int(raw_input('For how many years is this loan being borrowed? '))
 QUAL = raw_input('Are you pre-qualified for this loan? Please answer (Yes or y)'
                  'or (No or n). ')
+TOTAL = None
+INTEREST = None
 
 if PRIN >= 0 and PRIN <= 199999:
     if YEARS >= 1 and YEARS <= 15:
@@ -27,7 +28,7 @@ if PRIN >= 0 and PRIN <= 199999:
             INTEREST = .0577
         else:
             INTEREST = .0639
-if PRIN >= 200000 and PRIN <= 999999:
+elif PRIN >= 200000 and PRIN <= 999999:
     if YEARS >= 1 and YEARS <= 15:
         if QUAL is 'Yes' or QUAL is 'y':
             INTEREST = .0302
@@ -41,19 +42,18 @@ if PRIN >= 200000 and PRIN <= 999999:
     elif YEARS >= 21 and YEARS <= 30:
         if QUAL is 'Yes' or QUAL is 'y':
             INTEREST = .0466
-if PRIN >= 1000000:
+elif PRIN >= 1000000:
     if QUAL is 'Yes' or QUAL is 'y':
         if YEARS >= 1 and YEARS <= 15:
             INTEREST = .0205
         elif YEARS >= 16 and YEARS <= 20:
             INTEREST = .0262
 
-INTEREST = decimal.Decimal(INTEREST)
-TOTAL = PRIN *(1 + INTEREST / 12) ** (12 * YEARS)
-TOTAL = int(round(TOTAL))
-
+if INTEREST is not None:
+    INTEREST = decimal.Decimal(INTEREST)
+    TOTAL = int(round(PRIN *(1 + INTEREST / 12) ** (12 * YEARS)))
 REPORT = ('Loan Report for: {0:>15}\n---------------------------------------'
           '----\nPrincipal: {1:>21,.0f}\nDuration: {2:>19}yrs\n'
           'Pre-qualified?: {3:>16}\n'
-          '\nTotal: {4:>25,}').format(NAME, PRIN, YEARS, QUAL, TOTAL)
+          '\nTotal: {4:>25}').format(NAME, PRIN, YEARS, QUAL, TOTAL)
 print REPORT
