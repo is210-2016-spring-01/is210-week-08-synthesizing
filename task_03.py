f4577a157c8f3592eb2874595b82a1d0ad13dc23
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-22 23:11:06 +0000

    last last rev

--- task_03.py
+++ task_03.py
@@ -15,43 +15,44 @@ INTEREST = None
 if PRIN >= 0 and PRIN <= 199999:
     if YEARS >= 1 and YEARS <= 15:
         if QUAL is 'Yes' or QUAL is 'y':
-            INTEREST = .0363
+            INTEREST = '.0363'
         else:
-            INTEREST = .0465
+            INTEREST = '.0465'
     elif YEARS >= 16 and YEARS <= 20:
         if QUAL is 'Yes' or QUAL is 'y':
-            INTEREST = .0404
+            INTEREST = '.0404'
         else:
-            INTEREST = .0498
+            INTEREST = '.0498'
     elif YEARS >= 21 and YEARS <= 30:
         if QUAL is 'Yes' or QUAL is 'y':
-            INTEREST = .0577
+            INTEREST = '.0577'
         else:
-            INTEREST = .0639
+            INTEREST = '.0639'
 elif PRIN >= 200000 and PRIN <= 999999:
     if YEARS >= 1 and YEARS <= 15:
         if QUAL is 'Yes' or QUAL is 'y':
-            INTEREST = .0302
+            INTEREST = '.0302'
         else:
-            INTEREST = .0398
+            INTEREST = '.0398'
     elif YEARS >= 16 and YEARS <= 20:
         if QUAL is 'Yes' or QUAL is 'y':
-            INTEREST = .0327
+            INTEREST = '.0327'
         else:
-            INTEREST = .0408
+            INTEREST = '.0408'
     elif YEARS >= 21 and YEARS <= 30:
         if QUAL is 'Yes' or QUAL is 'y':
-            INTEREST = .0466
+            INTEREST = '.0466'
 elif PRIN >= 1000000:
     if QUAL is 'Yes' or QUAL is 'y':
         if YEARS >= 1 and YEARS <= 15:
-            INTEREST = .0205
+            INTEREST = '.0205'
         elif YEARS >= 16 and YEARS <= 20:
-            INTEREST = .0262
+            INTEREST = '.0262'
 
 if INTEREST is not None:
     INTEREST = decimal.Decimal(INTEREST)
     TOTAL = int(round(PRIN *(1 + INTEREST / 12) ** (12 * YEARS)))
+
 REPORT = ('Loan Report for: {0:>15}\n---------------------------------------'
           '----\nPrincipal: {1:>21,.0f}\nDuration: {2:>19}yrs\n'
           'Pre-qualified?: {3:>16}\n'
