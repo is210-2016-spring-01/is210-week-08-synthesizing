7fcb778aaa983a2b744c764a5c366fdeacb2fd38
jaiz25 <jaizelle@gmail.com>
2016-03-24 03:48:09 +0000

    Task 2 revised

--- task_02.py
+++ task_02.py
@@ -4,10 +4,10 @@
 
 
 QUESTION1 = raw_input('What day is it?: ')
-QUESTION1 = QUESTION1[0:3].lower() == 's'
+QUESTION1 = QUESTION1[:3].lower()
 QUESTION2 = int(raw_input('What time is it?: '))
 
-SNOOZE = (True if QUESTION1 or QUESTION2 < 600
+SNOOZE = (True if QUESTION1 == 'sat' or QUESTION1 == 'sun' or QUESTION2 < 600
           else False)
 
 if SNOOZE is False:
