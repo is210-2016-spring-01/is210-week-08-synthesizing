9964c66a5a19b8e3d994e46eac3ae4ae3a6bc83d
jaiz25 <jaizelle@gmail.com>
2016-03-24 03:17:42 +0000

    Task 2

--- task_02.py
+++ task_02.py
@@ -3,10 +3,10 @@
 """Task 2 docstring."""
 
 
-QUESTION1 = raw_input('What day is it?: ').lower()
-QUESTION1 = QUESTION1[0:3]
-QUESTION2 = raw_input('What time is it?: ')
+QUESTION1 = raw_input('What day is it?: ')
+QUESTION1 = QUESTION1[0:1].lower() == 's'
+QUESTION2 = int(raw_input('What time is it?: '))
 
-SNOOZE = (True if (QUESTION1 == 'sat' and 'sun') or QUESTION2 < 600
+SNOOZE = (True if QUESTION1 or QUESTION2 < 600
           else 'Beep! Beep! Beep! Beep! Beep!')
 print SNOOZE
