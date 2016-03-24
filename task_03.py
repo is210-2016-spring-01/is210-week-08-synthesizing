bb7534972b8354264343b2fc4283486d97fca3f0
Sylvia Guzikowski <xiran-aneki@live.com>
2016-03-24 04:29:08 +0000

    trying more and more and more - pylint2

--- task_03.py
+++ task_03.py
@@ -18,77 +18,6 @@ N = 12
 if 0 < LOAN <= 199999:
     if 0 < YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0363
-        else:
-            RATE = 0.0465
-    elif 16 <= YEARS <= 20:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0404
-        else:
-            RATE = 0.0498
-    elif 21 <= YEARS <= 30:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0577
-        else:
-            RATE = 0.0639
-elif 2000000 <= LOAN <= 999999:
-    if YEARS <= 15:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0302
-        else:
-            RATE = 0.0398
-    elif 16 <= YEARS <= 20:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0327
-        else:
-            RATE = 0.0408
-    elif 21 <= YEARS <= 30:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0466
-elif LOAN >= 1000000:
-    if YEARS <= 15:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0205
-    elif 16 <= YEARS <= 20:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = 0.0262
-    
-if RATE is not None:
-    Dec = Decimal(RATE)
-    NRATE = '{0:f}'.format(Dec)
-    TOTAL = int(LOAN * (1 + RATE/N)**(N*YEARS))
-    #TOTAL = int(round(LOAN * (1 + Decimal(Decimal(NRATE))/N)**(N*YEARS)))
-    LOAN = '{:0,d}'.format(LOAN)
-    STOTAL = '{:0,d}'.format(TOTAL)
-    REPORT = ('Loan report for: ' + NAME + ' \n'
-              + '-------------------------------------------------' + '\n'
-              + '       Principal:      ' + str(LOAN).rjust(20) + '\n'
-              + '       Duration:       ' + str(YEARS).rjust(20) + '\n'
-              + '       Pre-Qualified?: ' + QUALIFIED.rjust(20) + '\n'
-              + '\n'
-              + '       Total:          ' + STOTAL.rjust(20))
-    print REPORT
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Loans are great for students!!"""
-
-from decimal import Decimal
-
-NAME = raw_input('What is your name? ')
-LOAN = raw_input('What is the principal of the loan? ')
-YEARS = raw_input('How many years is this loan being borrowed? ')
-QUALIFIED = raw_input('Are you pre-qualified for this loan? ')
-
-LOAN = int(LOAN)
-YEARS = int(YEARS)
-RATE = None
-TOTAL = None
-N = 12
-
-if 0 < LOAN <= 199999:
-    if 0 < YEARS <= 15:
-        if QUALIFIED == 'Yes' or QUALIFIED == 'y':
             RATE = '0.0363'
         else:
             RATE = '0.0465'
@@ -139,3 +68,4 @@ if RATE is not None:
               + '\n'
               + '       Total:          ' + STOTAL.rjust(11))
     print REPORT
+
