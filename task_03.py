4756d2a4d311f09851743dd4b79e6711b749fbd3
Sylvia Guzikowski <xiran-aneki@live.com>
2016-03-24 03:46:17 +0000

    trying more after a break

--- task_03.py
+++ task_03.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Loans are great for studens!!"""
+"""Loans are great for students!!"""
 
 from decimal import Decimal
 NAME = raw_input('What is your name? ')
@@ -10,41 +10,41 @@ QUALIFIED = raw_input('Are you pre-qualified for this loan? ')
 
 LOAN = int(LOAN)
 YEARS = int(YEARS)
-#RATE = None
-#TOTAL = None
+RATE = None
+TOTAL = None
 
-if 0< LOAN <= 199999:
+if 0 < LOAN <= 199999:
     if YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0363'
+            RATE = 0.0363
         else:
-            RATE = '0.0465'
+            RATE = 0.0465
     elif 16 <= YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0404'
+            RATE = 0.0404
         else:
             RATE = '0.0498'
     elif 21 <= YEARS <= 30:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0577'
+            RATE = 0.0577
         else:
-            RATE = '0.0639'
+            RATE = 0.0639
     else:
         RATE = None
 elif 2000000 <= LOAN <= 999999:
     if YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0302'
+            RATE = 0.0302
         else:
-            RATE = '0.0398'
+            RATE = 0.0398
     elif 16 <= YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0327'
+            RATE = 0.0327
         else:
-            RATE = '0.0408'
+            RATE = 0.0408
     elif 21 <= YEARS <= 30:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0466'
+            RATE = 0.0466
         else:
             RATE = None
     else:
@@ -52,12 +52,12 @@ elif 2000000 <= LOAN <= 999999:
 elif LOAN >= 1000000:
     if YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0205'
+            RATE = 0.0205
         else:
             RATE = None
     elif 16 <= YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-            RATE = '0.0262'
+            RATE = 0.0262
         RATE = None
     else:
         RATE = None
